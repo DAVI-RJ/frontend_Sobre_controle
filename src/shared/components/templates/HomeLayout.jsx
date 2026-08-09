f3e94c1b0aa15b6f8abb64183f925c2d84f19bac
Davi RJ <daviribeirodjust007.28@gmail.com>
2026-08-09 13:10:05 -0300

    fix: atualizar dependências do MUI e corrigir formatação no HomeLayout

--- src/shared/components/templates/HomeLayout.jsx
+++ src/shared/components/templates/HomeLayout.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+
 import HeaderComponent from "../organisms/header/Header";
 import DashboardComponent from "../organisms/dashboard/Dashboard";
-import ContainerComponent from "../organisms/container/Container"
+import ContainerComponent from "../organisms/container/Container";
 import FooterComponent from "../organisms/footer/Footer";
 
-import "./home-layout.css"
+import "./home-layout.css";
 
-export default function HomeLayout ({children}) {
+export default function HomeLayout({ children }) {
   return (
     <div className="home-layout">
-      <HeaderComponent className="header-layout"/>
-      <DashboardComponent className="dashboard-layout"/>
-        <ContainerComponent>
-          {children}
-        </ContainerComponent> 
-      <FooterComponent className="footer-layout"/>
+      <div>
+        <HeaderComponent className="header-layout" />
+        <DashboardComponent className="dashboard-layout" />
+        <ContainerComponent>{children}</ContainerComponent>
+        <FooterComponent className="footer-layout" />
+      </div>
     </div>
-  )
+  );
 }
