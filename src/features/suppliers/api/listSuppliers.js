d85234139d1110c1cbee91f319cf562a882d9ce2
Davi RJ <daviribeirodjust007.28@gmail.com>
2026-08-25 00:19:36 -0300

    refactor: ajustando a endpoint de listagem do supplier

--- src/features/suppliers/api/listSuppliers.js
+++ src/features/suppliers/api/listSuppliers.js
@@ -1,6 +1,6 @@
 import { axiosInstance } from "@/core/http/axiosInstance";
 
 export async function listSuppliers() {
-  const response = await axiosInstance.get("/suppliers/list");
+  const response = await axiosInstance.get("/company/supplier/list");
   return response;
 }
