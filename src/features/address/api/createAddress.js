1c1f0eaf879ae261dc3df9e9d773a9582b218c9a
Davi RJ <daviribeirodjust007.28@gmail.com>
2026-08-27 00:52:38 -0300

    refactor: remover validação de dados no envio de endereço para a API

--- src/features/address/api/createAddress.js
+++ src/features/address/api/createAddress.js
@@ -1,9 +1,7 @@
 import { axiosInstance } from "@/core/http/axiosInstance";
-import { addressSchema } from "@/domain/schemas/addressSchema";
 
 export async function createAddress(data) {
-  const validatedData = addressSchema.parse(data);
-  const response = await axiosInstance.post("/company/address", validatedData);
+  const response = await axiosInstance.post("/company/address", data);
   console.log("Resposta da API:", response);
   return response;
 }
